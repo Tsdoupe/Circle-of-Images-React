bafa9df9ce74754476bfd3c1de125116213efa64
Tyler Doupe <tylerdoupe34@gmail.com>
2023-07-14 16:11:18 -0400

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import './App.css';
-import React from 'react';
-// import HomePage from './pages/HomePage';
+import {Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
 import GalleryPage from './pages/GalleryPage';
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import './App.css';
 
 function App() {
   return (
     <div className="App" style={{ backgroundColor: '#c8cca8'}}>
       <Header/>
-      {/* <HomePage /> */}
-      <GalleryPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='gallery' element={<GalleryPage />} />
+      </Routes>
       <Footer/>
     </div>
   );
